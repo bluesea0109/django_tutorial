4fec53ff8051318397dd06fc6666a8bbb4cdbe9e
bluesea0109 <50928728+bluesea0109@users.noreply.github.com>
2019-07-04 17:21:39 +0800

    2019-07-04

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -16,7 +17,14 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    # try:
+    #   question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #   raise Http404("Qustion does not exist")
+    # return HttpResponse("You're looking at question %s." % question_id)
+    def detail(request, question_id):
+      question = get_object_or_404(Question, pk=question_id)
+      return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "you're looking at the results of question %s."
